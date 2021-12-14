f77f8bd5ae86037a2a99ac15a9d2a5914b5eadb6
LJHong <362850915@qq.com>
2021-12-14 16:05:09 +0800

    asdfgh

--- api/envm/index.js
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/request.js";
-//环境新增修改
-export const envmSaveOrUpdate = (data) => {
-    return request({
-        url: "/blade-sd/envm/saveOrUpdate",
-        method: "post",
-        data,
-    });
-};
